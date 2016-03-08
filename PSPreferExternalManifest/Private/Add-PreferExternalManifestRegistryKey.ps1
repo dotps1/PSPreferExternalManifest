581b443ecda753021edbc5199d41dcd70f54905b
Thomas Malkewitz <get-alias@outlook.com>
2016-03-08 13:42:20 -0500

    Added reboot warning if the reg key was added.

--- PSPreferExternalManifest/Private/Add-PreferExternalManifestRegistryKey.ps1
+++ PSPreferExternalManifest/Private/Add-PreferExternalManifestRegistryKey.ps1
@@ -18,6 +18,8 @@
     End {
         try {
             New-ItemProperty @params | Out-Null
+
+            Write-Warning -Message "The registry value '$($params.Name)' was added to '$($params.Path)'.  You may need to reboot your computer for the changes to take affect."
         } catch {
             throw $_
         }
